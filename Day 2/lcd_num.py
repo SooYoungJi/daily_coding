a8e60776843681f2cd1a30ecdbd917c29539af11
SooYoungJi <jisooyoung97@gmail.com>
2023-06-03 13:10:48 +0900

    Create lcd_num.py

---
+++ Day 2/lcd_num.py
@@ -0,0 +1,20 @@
+'''
+<프로그래머스 순서쌍의 갯수>
+순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다. 
+자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+'''
+
+# 내 풀이
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        if n%i == 0:
+            answer += 1
+    return answer
+
+# 다른이 풀이
+def solution(n):
+    return len([number for number in range(1, n+1) if n%number == 0])
+'''
+내 풀이 한줄 요약 느낌
+'''

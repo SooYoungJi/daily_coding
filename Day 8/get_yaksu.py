53afe055b95c119297179f9a4225a183e8523384
SooYoungJi <jisooyoung97@gmail.com>
2023-06-11 14:59:19 +0900

    Create get_yaksu.py

---
+++ Day 8/get_yaksu.py
@@ -0,0 +1,12 @@
+'''
+<프로그래머스 약수 구하기>
+정수 n이 매개변수로 주어질 때, n의 약수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+'''
+
+# 내 풀이
+def solution(n):
+    return [i for i in range(1, n+1) if n%i==0]
+
+'''
+내 풀이 최고~~
+'''

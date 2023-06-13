f5bfbffb03f84a0daa766e125d892ec1edcfc9df
SooYoungJi <jisooyoung97@gmail.com>
2023-06-13 14:54:28 +0900

    Create odd_even.py

---
+++ Level 1/Day 1/odd_even.py
@@ -0,0 +1,14 @@
+'''
+<프로그래머스 짝수와 홀수>
+정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+'''
+# 내 풀이
+def solution(num):
+    if num%2==0:
+        return 'Even'
+    else:
+        return 'Odd'
+    
+'''
+1단계가 더 어려울 줄 알았는데 생각보다 0단계보다 쉬운것 같다...
+'''

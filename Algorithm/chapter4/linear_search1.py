fc6fb2360548c3f8777a7f1db20c1a404b2d4d14
SooYoungJi <jisooyoung97@gmail.com>
2023-06-13 14:54:24 +0900

    Create linear_search1.py

---
+++ Algorithm/chapter4/linear_search1.py
@@ -0,0 +1,14 @@
+'''
+<알고리즘 독학 4단원 선형검색>
+'''
+data = [50, 30, 90, 10, 20, 70, 60, 40, 80]
+found = False
+
+for i in range(len(data)):
+    if data[i] == 40:
+        print(i)
+        found = True
+        break
+
+if not found:
+    print('Not found')
